4a9ec2b504270afe6acb7a83b5c582495b664bc1
Terrance Kienga <terrance7004@gmail.com>
2023-07-28 00:16:41 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: 1st string to be compared
+ * @s2: 2nd string to be compared
+ * Return: Always zero (0) if both strings are the same.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+	int b = 0;
+
+	for (a = 0; s1[a] != '\0' && b == 0; a++)
+	{
+		b = s1[a] - s2[a];
+	}
+
+	return (b)
+}
