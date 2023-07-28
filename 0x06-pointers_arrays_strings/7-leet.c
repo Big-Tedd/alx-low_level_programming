4087be68d1e49dab2411ef03b036eba367ca6e4b
Terrance Kienga <terrance7004@gmail.com>
2023-07-28 14:47:14 -0700

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,32 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
+ *leet - a function that encodes a string into 1337.
+ *@s: string to be crypted .
+ *Return: pointer to s .
+ *Update V 1.0 : In this new code , i changed 2 arrays with 2 pointers, that
+ *are pointed to the BASS adresses of the 2 arrays.
+ *in addition to that , i assigned a pointer p to s.
  */
-int main(void)
+
+char *leet(char *s)
 {
-	char board[8][8] = {
-		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-	};
-	print_chessboard(board);
-	return (0);
+	char *m = "aeotl";
+	char *M = "AEOTL";
+	int x[] = {'4', '3', '0', '7', '1'};
+	int i;
+	char *p = s;
+
+	while (*s != '\0')
+	{
+
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == *(m + i) || *s == *(M + i))
+
+				*s = x[i];
+		}
+		s++;
+
+	}
+	return (p);
 }
